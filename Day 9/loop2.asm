d2544d403ac22efa1eec84b6805f02fd4eef2647
kavindiyashmi <kavindiyashmi99@gmail.com>
2025-05-30 04:35:07 +0000

    create loop2

---
+++ Day 9/loop2.asm
@@ -0,0 +1,39 @@
+;Use a loop to display numbers from 1 to 5(e.g.,1 2 3 4 5)
+
+.model small
+.stack 100h
+.data 
+ n db " $ "
+  
+.code
+
+main:
+ mov ax,@data 
+ mov ds,ax
+ 
+ mov cl,5
+ mov bl,1
+ 
+print_loop:
+ mov ah,02h
+ mov dl,bl
+ add dl,48
+ int 21h
+ 
+ mov dx,offset n
+ mov ah,09h
+ int 21h
+ 
+ mov dl,10  ;nextline
+ mov ah,02h
+ int 21h
+ 
+ 
+ inc bl
+ loop print_loop
+
+ mov ax,4c00h
+ int 21h
+ 
+ end main
+  
